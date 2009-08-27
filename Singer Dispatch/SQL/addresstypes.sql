7888a3f9ce46cb787776ad35640cd63beada5b1b
Simon Twogood <stwogood@siconix.com>
2009-08-27 14:59:27 -0600

    Removed the 'SQL' directory as the scripts in it our no longer needed now that the ne DatabaseBuilder class is done.

--- Singer Dispatch/SQL/addresstypes.sql
+++
@@ -1,3 +0,0 @@
-﻿insert into AddressTypes (Name) values ('Head Office');
-insert into AddressTypes (Name) values ('Local Office');
-insert into AddressTypes (Name) values ('Site Office');
--- Singer Dispatch/SQL/billingtypes.sql
+++
@@ -1,3 +0,0 @@
-﻿insert into BillingTypes (Name) values ('Per Item');
-insert into BillingTypes (Name) values ('Per Hour');
-insert into BillingTypes (Name) values ('Cost Included');
