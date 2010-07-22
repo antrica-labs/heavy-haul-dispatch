379b633970f9c51857a7433d2bf5b5b11f7db8ba
Simon Twogood <stwogood@siconix.com>
2010-07-22 16:59:20 -0600

    Added document printing stubs.

---
+++ Singer Dispatch/Printing/Documents/BillOfLadingDocument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingerDispatch.Printing.Documents
+{
+    class BillOfLadingDocument : IPrintDocument
+    {
+        public bool PrintMetric { get; set; }
+        public bool SpecializedDocument { get; set; }
+        
+        public string GenerateHTML(object entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Singer Dispatch/Printing/Documents/FuelReportDocument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingerDispatch.Printing.Documents
+{
+    class FuelReportDocument : IPrintDocument
+    {
+        public bool PrintMetric { get; set; }
+        public bool SpecializedDocument { get; set; }
+        
+        public string GenerateHTML(object entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
