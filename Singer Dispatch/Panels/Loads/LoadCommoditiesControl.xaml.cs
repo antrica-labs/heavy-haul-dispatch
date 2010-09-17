09fa34596b66488ffde15ef6f8dae86aaab104fa
Simon Twogood <stwogood@siconix.com>
2010-09-17 16:02:18 -0600

    The combo boxes on the new loaded commodities page are now functional.

--- Singer Dispatch/Panels/Loads/LoadCommoditiesControl.xaml.cs
+++ Singer Dispatch/Panels/Loads/LoadCommoditiesControl.xaml.cs
@@ -19,9 +19,52 @@ namespace SingerDispatch.Panels.Loads
     /// </summary>
     public partial class LoadCommoditiesControl
     {
+        public SingerDispatchDataContext Database { get; set; }
+
         public LoadCommoditiesControl()
         {
             InitializeComponent();
+        }
+
+        private void ThePanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateComboBoxes();
+        }
+
+        protected override void SelectedLoadChanged(Load newValue, Load oldValue)
+        {
+            base.SelectedLoadChanged(newValue, oldValue);
+
+        }
+
+        private void UpdateComboBoxes()
+        {
+            if (SelectedLoad == null) return;
+
+            var companies = from c in Database.Companies select c;
+
+            cmbShipperCompanies.ItemsSource = companies;
+            cmbConsigneeCompanies.ItemsSource = companies;
+            cmbLoadingSiteContactCompanies.ItemsSource = companies;
+            cmbLoadingContactCompanies.ItemsSource = companies;
+            cmbUnloadingSiteContactCompanies.ItemsSource = companies;
+            cmbUnloadingContactCompanies.ItemsSource = companies;
+
+            var methods = from m in Database.LoadUnloadMethods select m;
+
+            cmbLoadMethods.ItemsSource = methods;
+            cmbUnloadMethods.ItemsSource = methods;
+
+            if (cmbLoadingSiteContactCompanies.SelectedItem == null)
+                cmbLoadingSiteContactCompanies.SelectedItem = SelectedCompany;
+
+            if (cmbUnloadingSiteContactCompanies.SelectedItem == null)
+                cmbUnloadingSiteContactCompanies.SelectedItem = SelectedCompany;
+
+            var provinces = from p in Database.ProvincesAndStates select p;
+
+            cmbLoadingProvinces.ItemsSource = provinces;
+            cmbUnloadingProvinces.ItemsSource = provinces;
         }
 
         private void CommodityName_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -49,34 +92,49 @@ namespace SingerDispatch.Panels.Loads
 
         }
 
-        private void cmbLoadingContactCompanies_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-        
-        private void cmbUnloadingContactCompanies_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-        
         private void cmbLoadingSiteContactCompanies_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            var company = (Company)cmbLoadingSiteContactCompanies.SelectedItem;
 
+            cmbLoadingSiteContacts.ItemsSource = (company == null) ? null : (from c in Database.Contacts where c.Address.CompanyID == company.ID orderby c.FirstName, c.LastName select c).ToList();
         }
-        
+
         private void cmbUnloadingSiteContactCompanies_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            var company = (Company)cmbUnloadingSiteContactCompanies.SelectedItem;
 
+            cmbUnloadingSiteContacts.ItemsSource = (company == null) ? null : (from c in Database.Contacts where c.Address.CompanyID == company.ID orderby c.FirstName, c.LastName select c).ToList();
+        }
+
+        private void cmbLoadingContactCompanies_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var company = (Company)cmbLoadingContactCompanies.SelectedItem;
+
+            cmbLoadingContacts.ItemsSource = (company == null) ? null : (from c in Database.Contacts where c.Address.CompanyID == company.ID orderby c.FirstName, c.LastName select c).ToList();
+        }
+
+        private void cmbUnloadingContactCompanies_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var company = (Company)cmbUnloadingContactCompanies.SelectedItem;
+
+            cmbUnloadingContacts.ItemsSource = (company == null) ? null : (from c in Database.Contacts where c.Address.CompanyID == company.ID orderby c.FirstName, c.LastName select c).ToList();
         }
 
         private void ShipperCompany_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            var cmb = (ComboBox)sender;
+            var company = (Company)cmb.SelectedItem;
 
+            cmbShipperAddresses.ItemsSource = (company != null) ? company.Addresses : null;
         }
 
         private void ConsigneeCompany_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            var cmb = (ComboBox)sender;
+            var company = (Company)cmb.SelectedItem;
 
+            cmbConsigneeAddresses.ItemsSource = (company != null) ? company.Addresses : null;
         }
+        
     }
 }
