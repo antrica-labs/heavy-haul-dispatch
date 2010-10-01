83ddfb83e3600894320c70a49109d99202d39ea8
Simon Twogood <stwogood@siconix.com>
2010-10-01 17:51:33 -0600

    Possible solution for informing users when they have typed an invalid selection in an editable combobox.

---
+++ Singer Dispatch/Controls/Validators/SelectionExistValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace SingerDispatch.Controls.Validators
+{
+    class SelectionExistValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            var text = combo.Text;
+            var selection = combo.SelectedItem;
+
+            if (selection == null && text.Length > 0)
+                return new ValidationResult(false, "Item not found");
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
