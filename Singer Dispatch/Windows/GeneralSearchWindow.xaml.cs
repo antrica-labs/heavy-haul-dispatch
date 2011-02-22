24477ed9cb650f7b149ab5375bedfed6bfc22628
Simon Twogood <simon.twogood@gmail.com>
2011-02-21 21:15:33 -0700

    Added more search stuff.

--- Singer Dispatch/Windows/GeneralSearchWindow.xaml.cs
+++ Singer Dispatch/Windows/GeneralSearchWindow.xaml.cs
@@ -17,10 +17,7 @@ namespace SingerDispatch.Windows
     /// Interaction logic for GeneralSearchWindow.xaml
     /// </summary>
     public partial class GeneralSearchWindow : Window
-    {
-        private int TAB_JOBS = 0;
-        private int TAB_QUOTE = 1;
-        
+    {        
         private object SelectedEntity;
 
         public GeneralSearchWindow()
@@ -68,7 +65,6 @@ namespace SingerDispatch.Windows
         private void FindJobs(string term, string company, DateTime? start, DateTime? end)
         {
             var database = new SingerDispatchDataContext();
-
             var jobs = from j in database.Jobs select j;
 
             if (!string.IsNullOrWhiteSpace(term))
@@ -86,32 +82,148 @@ namespace SingerDispatch.Windows
 
         private void FindLoads(string term, string company, DateTime start, DateTime end)
         {
+            var database = new SingerDispatchDataContext();
+            var loads = from l in database.Loads select l;
 
+            if (!string.IsNullOrWhiteSpace(term))
+            {
+                //loads = from l in database.Loads where l.
+            }
+
+            if (!string.IsNullOrWhiteSpace(company))
+            {
+                loads = from l in loads where l.Job.Company.Name.ToUpper().Contains(company.ToUpper()) || (l.Job.CareOfCompany != null && l.Job.CareOfCompany.Name.ToUpper().Contains(company.ToUpper())) select l;
+            }
+
+            dgLoads.ItemsSource = loads;
         }
 
         private void FindDispatches(string term, string company, DateTime start, DateTime end)
         {
+            var database = new SingerDispatchDataContext();
+            var dispatches = from d in database.Dispatches select d;
 
+            if (!string.IsNullOrWhiteSpace(term))
+            {
+                
+            }
+
+            if (!string.IsNullOrWhiteSpace(company))
+            {
+                dispatches = from d in dispatches where d.Load.Job.Company.Name.ToUpper().Contains(company.ToUpper()) || (d.Load.Job.CareOfCompany != null && d.Load.Job.CareOfCompany.Name.ToUpper().Contains(company.ToUpper())) select d;
+            }
+
+            if (start != null)
+            {
+                dispatches = from d in dispatches where d.MeetingDate >= start select d;
+            }
+
+            if (end != null)
+            {
+                dispatches = from d in dispatches where d.MeetingDate <= end select d;
+            }
+
+            dgDispatches.ItemsSource = dispatches;
         }
 
         private void FindQuotes(string term, string company, DateTime start, DateTime end)
         {
+            var database = new SingerDispatchDataContext();
+            var quotes = from q in database.Quotes select q;
 
+            if (!string.IsNullOrWhiteSpace(term))
+            {
+                quotes = from q in quotes where q.Description.ToUpper().Contains(term.ToUpper()) select q;
+            }
+
+            if (!string.IsNullOrWhiteSpace(company))
+            {
+                quotes = from q in quotes where q.Company.Name.ToUpper().Contains(company.ToUpper()) || (q.CareOfCompany != null && q.CareOfCompany.Name.ToUpper().Contains(company.ToUpper())) select q;
+            }
+
+            if (start != null)
+            {
+                quotes = from q in quotes where q.StartDate >= start || q.CreationDate >= start select q;
+            }
+
+            if (end != null)
+            {
+                quotes = from q in quotes where q.StartDate <= end || q.CreationDate <= end select q;
+            }
         }
 
         private void FindInvoices(string term, string company, DateTime start, DateTime end)
         {
+            var database = new SingerDispatchDataContext();
+            var invoices = from i in database.Invoices select i;
 
+            if (!string.IsNullOrWhiteSpace(term))
+            {
+
+            }
+
+            if (!string.IsNullOrWhiteSpace(company))
+            {
+                invoices = from i in invoices where i.Company.Name.ToUpper().Contains(company.ToUpper()) select i;
+            }
+
+            if (start != null)
+            {
+                invoices = from i in invoices where i.InvoiceDate >= start select i;
+            }
+
+            if (end != null)
+            {
+                invoices = from i in invoices where i.InvoiceDate <= end select i;
+            }
+
+            dgInvoices.ItemsSource = invoices;
         }
 
         private void FindThirdPartyServices(string term, string company, DateTime start, DateTime end)
         {
+            var database = new SingerDispatchDataContext();
+            var services = from s in database.ThirdPartyServices select s;
 
+            if (!string.IsNullOrWhiteSpace(term))
+            {
+                services = from s in services where s.Reference != null && s.Reference.ToUpper().Contains(term.ToUpper()) select s;
+            }
+
+            if (!string.IsNullOrWhiteSpace(company))
+            {
+                services = from s in services where s.Company != null && s.Company.Name.ToUpper().Contains(company.ToUpper()) select s;
+            }
+
+            if (start != null)
+            {
+                services = from s in services where s.ServiceDate >= start select s;
+            }
+
+            if (end != null)
+            {
+                services = from s in services where s.ServiceDate <= end select s;
+            }
+
+            dgServices.ItemsSource = services;
         }
 
         private void FindPermits(string term, string company, DateTime start, DateTime end)
         {
+            var database = new SingerDispatchDataContext();
+            var permits = from p in database.Permits select p;
 
+            if (!string.IsNullOrWhiteSpace(term))
+            {
+                permits = from p in permits where p.Reference != null && p.Reference.ToUpper().Contains(term.ToUpper()) select p;
+            }
+
+            if (!string.IsNullOrWhiteSpace(company))
+            {
+                permits = from p in permits where p.IssuingCompany != null && p.IssuingCompany.Name.ToUpper().Contains(company.ToUpper()) select p;                          
+            }
+
+            dgPermits.ItemsSource = permits;
         }
     }
 }
